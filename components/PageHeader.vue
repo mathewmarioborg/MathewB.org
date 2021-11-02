077c31a68b868a24e986867dd006b2e45c7dff26
mat4539 <mborg0020@gmail.com>
2021-11-02 14:20:24 -0400

    added header

---
+++ components/PageHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <p>{{subtitle}}</p>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageHeader",
+  props: {
+    title: {
+      type: String,
+    },
+    subtitle:{
+      type: String,
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    padding-top: 0.5em;
+    font-size: 36px;
+    color: aliceblue;
+    letter-spacing: 4px;
+  }
+
+  p {
+    text-align: center;
+    font-size: 18px;
+    color: aliceblue;
+    letter-spacing: 4px;
+  }
+
+  hr {
+    width: 60%;
+    display: block;
+    margin: 0.5em auto;
+    border: none;
+    height: 3px;
+    border-radius: 2px;
+    background-color: #35495e;
+  }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,21 @@
 <template>
   <div class='page'>
-    <div class='test1'>
-      <container1 class='test1' title='Test 1' body='Test 222222222222222222222222222222222222222222222222222222222222  info'/>
-    </div>
+    <PageHeader title="Mathew Borg" subtitle="My Personal Portfolio"/>
+    <container1 class='test1' title='Test 1' body='Test 222222222222222222222222222222222222222222222222222222222222  info'/>
+    <container1 title="Test 2" body="testvfkjrebhvpkj fvneovneroibv e erer ger oiergiouergjheroig eri gergireogie egoer go eirger gerig erf hdsfh dskjfh sdkfh skf shjkfsdhkjf hsdkf hskdfh kjsdkjkjlsdafl kg"/>
   </div>
 </template>
 
 <script>
 import Container1 from '../components/container1'
+import PageHeader from "../components/PageHeader";
 export default {
-  components: { Container1 }
+  components: {PageHeader, Container1 }
 }
 </script>
 
 <style>
 
-.test1 {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: center;
-}
+
 
 </style>
