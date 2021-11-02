3ae53cc46c1689d8419accb01fd0ad8ddbd6c4dc
mat4539 <mborg0020@gmail.com>
2021-11-02 12:24:14 -0400

    All Files

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='page'>
+    <div class='test1'>
+      <container1 class='test1' title='Test 1' body='Test 222222222222222222222222222222222222222222222222222222222222  info'/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Container1 from '../components/container1'
+export default {
+  components: { Container1 }
+}
+</script>
+
+<style>
+
+.test1 {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+
+</style>
