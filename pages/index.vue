7f73c64ff16c1d2732906290c7f524f8eefb0497
mat4539 <mborg0020@gmail.com>
2021-12-08 23:34:12 -0500

    created container3.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <PageHeader title="Mathew Borg" subtitle="My Personal Portfolio"/>
     <container1 class='container' title='Test 1' body='Test 222222222222222222222222222222222222222222222222222222222222  info'/>
     <container1 title="Test 2" img='Mathew_Borg_Final_White.jpg' body="testvfkjrebhvpkj fvneovneroibv e erer ger oiergiouergjheroig eri gergirtbrtnrfgnghfnghfnghfnghfn thtr htreh rh rthrt hyrt htrreogie egoer go eirger gerig erf hdsfh dskjfh sdkfh skf shjkfsdhkjf hsdkf hskdfh kjsdkjkjlsdafl kg"/>
-    <container2 title="Test 2" img='Mathew_Borg_Final_White.jpg' body="testvfkjrebhvpkj fvneovneroibv e erer ger oiergiouergjheroig eri gergirtbrtnrfgnghfnghfnghfnghfn thtr htreh rh rthrt hyrt htrreogie egoer go eirger gerig erf hdsfh dskjfh sdkfh skf shjkfsdhkjf hsdkf hskdfh kjsdkjkjlsdafl kg"/>
+    <container2 title="Test 3" img='Mathew_Borg_Final_White.jpg' body="testvfkjrebhvpkj fvneovneroibv e erer ger oiergiouergjheroig eri gergirtbrtnrfgnghfnghfnghfnghfn thtr htreh rh rthrt hyrt htrreogie egoer go eirger gerig erf hdsfh dskjfh sdkfh skf shjkfsdhkjf hsdkf hskdfh kjsdkjkjlsdafl kg"/>
+    <container3 title="Test 4" img='Mathew_Borg_Final_White.jpg' body="testvfkjrebhvpkj fvneovneroibv e erer ger oiergiouergjheroig eri gergirtbrtnrfgnghfnghfnghfnghfn thtr htreh rh rthrt hyrt htrreogie egoer go eirger gerig erf hdsfh dskjfh sdkfh skf shjkfsdhkjf hsdkf hskdfh kjsdkjkjlsdafl kg"/>
+
   </div>
 </template>
 
