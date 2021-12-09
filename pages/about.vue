1f31e491bdfd59b9c1162d04c4847448c9011fa9
mat4539 <mborg0020@gmail.com>
2021-12-09 00:31:26 -0500

    added images

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="page">
     <PageHeader title="About" subtitle="All About Me"/>
+    <container2 title="Background Information" img='IMG_1407.jpg' body="My name is Mathew Borg and I’m 18 years old. I have created this website to demonstrate my web development abilities and as a master portfolio to show off all my work. I have taken part in many great projects in my life thus far and I will continue to involve myself in projects to increase my knowledge and make a difference in the world."></container2>
   </div>
 </template>
 
