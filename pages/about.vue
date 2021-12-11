6133d9c461e3d0080657fdc92e8594fefe52ce1f
mat4539 <mborg0020@gmail.com>
2021-12-10 19:51:56 -0500

    Adding info to pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="page">
+  <div class="page-container">
     <PageHeader title="About" subtitle="All About Me"/>
-    <container2 title="Background Information" img='IMG_1407.jpg' body="My name is Mathew Borg and I’m 18 years old. I have created this website to demonstrate my web development abilities and as a master portfolio to show off all my work. I have taken part in many great projects in my life thus far and I will continue to involve myself in projects to increase my knowledge and make a difference in the world."></container2>
+    <textBox title="Information On my life And Goals" body="My name is Mathew Borg and I’m 18 years old. I am currently attending Humber College and I am currently working towards getting an advanced diploma in computer programming & analysis. I was born in Alliston Ontario but I have grown up in Beeton Ontario my whole life. My cultural background is Maltese and Italian. The Strengths and skills I have are my computer programming, web development and skills in small electrical components. These are my areas of strengths because they are some of the things I love working on in my spare time which allows me to be creative and challenge my thinking skills. With these skills I am often faced with challenges and whenever a challenge comes up in my life I will never give up on it and I will work on it until I overcome it. I love challenges because  they force me to come up with different ways to look at situations and push me to learn more information. Life is full of many achievements. These can be as small as learning how to tie our shoes or as big as graduating, getting married, having a rewarding career or having children. To me it is important to set goals that are important to you so you can push yourself to achieve your goals. Some of my most proud achievements are: on June 27 2017 I graduated from Monsignor J.E. Ronan Catholic School, on January 2018 and January 2019 when I was on the science honor roll at Banting Memorial High School, on September 2018 and September 2019 when I received Honor Roll at banting memorial High School for having an overall grade average of over 80% in grade 9 over a 90% grade average in grade 10 and for, helping my FIRST Robotics Team 7200 to win the Autonomous Programing awards at the Georgen and Ryerson events in spring 2019. These are all good achievements as they show progression but I will always look to use my strengths to help make an impact on the world and to better the lives of others. At this point in time one of my biggest goals that I am looking to accomplish is to graduate Banting Memorial High School with an overall grade average of over 80% in grade 12 and to go on to College for Computer Science. Another goal I am looking to accomplish by 2021 is to have started my own development company. I'm sure if I continue my efforts I will be able to accomplish these goals by working hard."/>
+
   </div>
 </template>
 
 <script>
+import PageHeader from "@/components/PageHeader";
+import textBox from "@/components/textBox";
+import textBoxRight from "@/components/textBoxRight";
+import textBoxLeft from "@/components/textBoxLeft";
 export default {
+  components: {PageHeader, textBox, textBoxRight, textBoxLeft},
   name: 'about'
 }
 </script>
 
 <style scoped>
-.page{
-  display: flex;
-  flex-direction: column;
-  -ms-flex-align: center;
-}
+
 </style>
 
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="page">
+  <div class="page-container">
     <PageHeader title="Portfolio" subtitle="A Compilation Of All My Work"/>
+    <TextBoxLeft title="Web Development" body="This website you are currently visiting has been coded using the nuxt.js framework which was built off of vue.js. This has been done to show my abilities of using html css and custom web frameworks. This site is used to prove that I can use html and other web tools to construct a website. In addition to this I have also done web work using wordpress and php programming. That website was made to be a skeleton of this website so all the information will be identical but the layout and web design will be different. wp.mathewborg.com was also made to show my clients an example of a website that I could create for them. I also own borgdevelopment.ca and this is my main website for my business Borg Development. That site has also been designed using wordpress to show my clients another example of a website I can create. I also own a third domain mathewb.org which I use for testing all kinds of web projects so feel free to check it out and see what i'm currently working on." :image="require(`~/assets/IMG-003-Graffiti Mathew_2021-08-20.jpg`)"/>
+    <TextBoxRight title="First Robotics" body="Robotics has become one of my greatest passions over the last 4 years of my life. Ever since I joined the First Robotics Team 7200 in 2018 I have taken such an interest in robotics and invested many hours of my life working on robotic technologies. The thing I love most of all about robotics is programming. I have always been fascinated on how programming can make things work and how there are different methods to make things run more effectively. Within my last 4 years on First Robotics Team 7200 I have increased my knowledge on java programing, autonomous vision systems, and even learned about how to apply physics to robotic programming." :image="require(`~/assets/IMG-004-Graffiti Mathew_2021-08-20.jpg`)"/>
   </div>
 </template>
 
 <script>
+import PageHeader from "@/components/PageHeader";
+import textBox from "@/components/textBox";
+import textBoxRight from "@/components/textBoxRight";
+import textBoxLeft from "@/components/textBoxLeft";
 export default {
+  components: {PageHeader, textBox, textBoxRight, textBoxLeft},
   name: 'portfolio'
 }
 </script>
 
 <style scoped>
-.page{
-  display: flex;
-  flex-direction: column;
-  -ms-flex-align: center;
-}
+
 </style>
