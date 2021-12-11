cb39e993e414ba970537c714f10335479eade12e
mat4539 <mborg0020@gmail.com>
2021-12-10 19:20:07 -0500

    Added Home Page context

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
   <div class='page-container'>
     <PageHeader title="Mathew Borg" subtitle="My Personal Portfolio"/>
-    <textBox :image="require(`~/assets/IMG-005-Graffiti Mathew_2021-08-20.jpg`)" title="Test" body="test2"></textBox>
-    <textBox title="Test" body="test2"></textBox>
-    <textBoxRight title="testOne" body="testTwo" :image="require(`~/assets/IMG-005-Graffiti Mathew_2021-08-20.jpg`)"></textBoxRight>
-    <textBoxLeft title="TestOneMillion" body="testTwoMillion" :image="require(`~/assets/IMG-001-Graffiti Mathew_2021-08-20.jpg`)"></textBoxLeft>
+    <textBoxRight title="Welcome To MathewBorg.com" body="This website you are currently visiting has been coded using the nuxt.js framework which was built off of vue.js. This has been done to show my abilities of using html css and custom web frameworks. This site is used to prove that I can use html and other web tools to construct a website. In addition to this I have also done web work using wordpress and php programming. " :image="require(`~/assets/IMG-001-Graffiti Mathew_2021-08-20.jpg`)"></textBoxRight>
+    <textBoxLeft title="My Background Information" body="My name is Mathew Borg and I’m 18 years old. I have created this website to demonstrate my web development abilities and as a master portfolio to show off all my work. I have taken part in many great projects in my life thus far and I will continue to involve myself in projects to increase my knowledge and make a difference in the world." :image="require(`~/assets/IMG-002-Graffiti Mathew_2021-08-20.jpg`)"></textBoxLeft>
   </div>
 </template>
 
